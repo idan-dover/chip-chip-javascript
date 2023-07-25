fc836f22be0b10ab0d818ba6275b2e97b2df8c1f
Idan Dover <idan.dava@gmail.com>
2023-07-25 10:47:10 +0300

    migrated to axios

--- src/services/apiAdmin.js
+++ src/services/apiAdmin.js
@@ -1,170 +1,268 @@
+import axios from "axios";
 import { ADMIN_URL } from "./URLs";
-//TODO: finish replacing apiAdmin to axios
+
+// export async function getAllCompanies() {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/companies`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: token,
+//       },
+//     });
+//     const data = await res.json();
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+
+//     return data;
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function getAllCompanies() {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/companies`, {
-      method: "GET",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .get(`${ADMIN_URL}/companies`, {
       headers: {
         Authorization: token,
       },
-    });
-    const data = await res.json();
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-
-    return data;
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
+
+// export async function addCompany(company) {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/companies`, {
+//       method: "POST",
+//       headers: {
+//         Authorization: token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(company),
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function addCompany(company) {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/companies`, {
-      method: "POST",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .post(`${ADMIN_URL}/companies`, company, {
       headers: {
         Authorization: token,
-        "Content-Type": "application/json",
       },
-      body: JSON.stringify(company),
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
+
+// export async function updateCompany(data) {
+//   const { companyId, company } = data;
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/companies/${companyId}`, {
+//       method: "PUT",
+//       headers: {
+//         Authorization: token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(company),
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function updateCompany(data) {
   const { companyId, company } = data;
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/companies/${companyId}`, {
-      method: "PUT",
-      headers: {
-        Authorization: token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(company),
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
-}
-
-export async function deleteCompany(id) {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/companies/${id}`, {
-      method: "DELETE",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .put(`${ADMIN_URL}/companies/${companyId}`, company, {
       headers: {
         Authorization: token,
       },
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
+
+// export async function deleteCompany(id) {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/companies/${id}`, {
+//       method: "DELETE",
+//       headers: {
+//         Authorization: token,
+//       },
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
+
+export async function deleteCompany(companyId) {
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .delete(`${ADMIN_URL}/companies/${companyId}`, {
+      headers: {
+        Authorization: token,
+      },
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
+}
+
+// export async function getAllCustomers() {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/customers`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: token,
+//       },
+//     });
+//     const data = await res.json();
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+
+//     return data;
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function getAllCustomers() {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/customers`, {
-      method: "GET",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .get(`${ADMIN_URL}/customers`, {
       headers: {
         Authorization: token,
       },
-    });
-    const data = await res.json();
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-
-    return data;
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
+
+// export async function addCustomer(customer) {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/customers`, {
+//       method: "POST",
+//       headers: {
+//         Authorization: token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(customer),
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function addCustomer(customer) {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/customers`, {
-      method: "POST",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .post(`${ADMIN_URL}/customers`, customer, {
       headers: {
         Authorization: token,
-        "Content-Type": "application/json",
       },
-      body: JSON.stringify(customer),
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
+
+// export async function updateCustomer(data) {
+//   const { customerId, customer } = data;
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/customers/${customerId}`, {
+//       method: "PUT",
+//       headers: {
+//         Authorization: token,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(customer),
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
 
 export async function updateCustomer(data) {
   const { customerId, customer } = data;
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/customers/${customerId}`, {
-      method: "PUT",
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .put(`${ADMIN_URL}/customers/${customerId}`, customer, {
       headers: {
         Authorization: token,
-        "Content-Type": "application/json",
       },
-      body: JSON.stringify(customer),
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
 
-export async function deleteCustomer(id) {
-  try {
-    const token = JSON.parse(localStorage.getItem("auth")).token;
-    const res = await fetch(`${ADMIN_URL}/customers/${id}`, {
-      method: "DELETE",
+// export async function deleteCustomer(id) {
+//   try {
+//     const token = JSON.parse(localStorage.getItem("auth")).token;
+//     const res = await fetch(`${ADMIN_URL}/customers/${id}`, {
+//       method: "DELETE",
+//       headers: {
+//         Authorization: token,
+//       },
+//     });
+
+//     if (!res.ok) {
+//       const err = await res.json();
+//       throw new Error(err.description);
+//     }
+//   } catch (err) {
+//     throw new Error(err);
+//   }
+// }
+
+export async function deleteCustomer(customerId) {
+  const token = JSON.parse(localStorage.getItem("auth")).token;
+  return axios
+    .delete(`${ADMIN_URL}/customers/${customerId}`, {
       headers: {
         Authorization: token,
       },
-    });
-
-    if (!res.ok) {
-      const err = await res.json();
-      throw new Error(err.description);
-    }
-  } catch (err) {
-    throw new Error(err);
-  }
+    })
+    .then((res) => res.data)
+    .catch((error) => new Error(error.response.data.description));
 }
